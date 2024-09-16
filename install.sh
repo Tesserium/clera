d9b3ae2a96f63474e2adeb79c748983344d7c2ca
code2828 <stevenhaocheng@163.com>
2024-09-16 20:41:20 +0800

    install?

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+curl https://circleci.com/api/v1.1/project/github/Tesserium/clera/latest/artifacts \
+  | grep -o -E 'https://([^"]*)' \
+  | wget --verbose --input-file -
+mv clera /usr/local/bin
+
