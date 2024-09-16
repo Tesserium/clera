6e5fb6ece7d5290dc16f27a8a160290dd9e9e98e
code2828 <stevenhaocheng@163.com>
2024-09-16 21:20:22 +0800

    fixed missing slash

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ curl https://circleci.com/api/v1.1/project/github/Tesserium/clera/latest/artifac
 mv clera /usr/local/bin
 ln /usr/local/bin/clera /usr/local/bin/claer
 ln /usr/local/bin/clera /usr/local/bin/celar
-ln /usr/local/bin/clera /usr/local/bin lcear
+ln /usr/local/bin/clera /usr/local/bin/lcear
