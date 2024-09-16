b3e0b5c2a4f56529d411d34b60044a3ba5a47d25
code2828 <stevenhaocheng@163.com>
2024-09-16 21:16:30 +0800

    also claer, celar and lcear

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ curl https://circleci.com/api/v1.1/project/github/Tesserium/clera/latest/artifac
   | grep -o -E 'https://([^"]*)' \
   | wget --verbose --input-file -
 mv clera /usr/local/bin
-
+ln /usr/local/bin/clera /usr/local/bin/claer
+ln /usr/local/bin/clera /usr/local/bin/celar
+ln /usr/local/bin/clera /usr/local/bin lcear
