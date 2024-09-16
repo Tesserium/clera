f3a62f5c0da7b1a093fc5df44dfa512a0eccb997
code2828 <stevenhaocheng@163.com>
2024-09-16 21:33:17 +0800

    exe

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+rm -rf clera
 curl https://circleci.com/api/v1.1/project/github/Tesserium/clera/latest/artifacts \
   | grep -o -E 'https://([^"]*)' \
   | wget --verbose --input-file -
-mv clera /usr/local/bin
+mv clera /usr/local/bin/clera
 ln /usr/local/bin/clera /usr/local/bin/claer
 ln /usr/local/bin/clera /usr/local/bin/celar
 ln /usr/local/bin/clera /usr/local/bin/lcear
+chmod +x /usr/local/bin/clera
+chmod +x /usr/local/bin/claer
+chmod +x /usr/local/bin/celar
+chmod +x /usr/local/bin/lcear
