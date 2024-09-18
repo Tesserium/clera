cad0b7baf385d75b8a7e3dc08f6c786aae3c2975
Haocheng Zhang <50510911+code2828@users.noreply.github.com>
2024-09-18 11:17:33 +0800

    Force mv & ln

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ rm -rf clera
 curl https://circleci.com/api/v1.1/project/github/Tesserium/clera/latest/artifacts \
   | grep -o -E 'https://([^"]*)' \
   | wget --verbose --input-file -
-sudo mv clera /usr/local/bin/clera
-sudo ln /usr/local/bin/clera /usr/local/bin/claer
-sudo ln /usr/local/bin/clera /usr/local/bin/celar
-sudo ln /usr/local/bin/clera /usr/local/bin/lcear
+sudo mv -f clera /usr/local/bin/clera
+sudo ln -f /usr/local/bin/clera /usr/local/bin/claer
+sudo ln -f /usr/local/bin/clera /usr/local/bin/celar
+sudo ln -f /usr/local/bin/clera /usr/local/bin/lcear
 sudo chmod +x /usr/local/bin/clera
 sudo chmod +x /usr/local/bin/claer
 sudo chmod +x /usr/local/bin/celar
